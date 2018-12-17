37d9ee74fa76c7f476859f74e3ade569312618a4
Andrej Šebeň <andrej.seben@millennium.sk>
2018-12-17 12:34:49 +0100

    Added MobX
    If you want working version, download master branch.

--- App-backup.js
+++ App-backup.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import { Alert, TextInput, View, TouchableHighlight, Text, AsyncStorage} from 'react-native';
 import CustomFlatList  from './src/components/ResourcesList.js';
 import StylesJS from './src/features/styles.js';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
 
 export default class App extends React.Component{
     constructor(props){
@@ -23,6 +21,27 @@ export default class App extends React.Component{
         });
     }
 
+    _saveData = async () => {
+        try{
+            await AsyncStorage.setItem(this.state.serverURL);
+        }
+        catch (error){
+            Alert.alert(error);
+        }
+    }
+
+    _retrieveData = async () => {
+        try{
+            const value = await AsyncStorage.getItem('Servers');
+            if (value !== null ) {
+                Alert.alert(value);
+            }
+        }
+        catch (error){
+            Alert.alert(error);
+        }
+    }
+
     getResources(){
         let URL = `http://${this.state.serverURL}/api/data?ident=villages`;
         
@@ -37,12 +56,8 @@ export default class App extends React.Component{
             <View style={StylesJS.main}>
                 <Text style={StylesJS.header}>Api bot helper</Text>
                 <TextInput style={StylesJS.searchInput} placeholder="http://" onChange={this.handleChange}/>
-
-                <TouchableHighlight style={StylesJS.button}  onPress={serverList.addURL(this.state.serverURL)}>
-                    <Text style={StylesJS.buttonText}>Add server</Text>
-                </TouchableHighlight>
-                <TouchableHighlight style={StylesJS.button}  onPress={this._retrieveData}>
-                    <Text style={StylesJS.buttonText}>Retrieve server list</Text>
+                <TouchableHighlight style={StylesJS.button}  onPress={this.getResources}>
+                    <Text style={StylesJS.buttonText}>Get resources</Text>
                 </TouchableHighlight>
 
                 <CustomFlatList itemList= {this.state.dataSource}/>
@@ -50,23 +65,3 @@ export default class App extends React.Component{
         )
     }
 }
-
-class ServerList {
-    serverURLs = [];
-
-    addURL(url){
-        this.serverURLs.push({
-            urlMobx: url
-        });
-        console.log(this.serverURLs[0].urlMobx);
-    }
-
-    getAllUrls(){
-        if(this.serverURLs.length === 0){
-            return "<none>";
-        }
-        return `Server URLs: "${this.serverURLs[0].urlMobx}".`
-    }
-}
-
-const serverList = new ServerList();
